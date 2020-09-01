c14917b167efa5354925dc161a6b1b3aad3edbb7
Ricky Shah <ricky@Rickys-MacBook-Pro.local>
2020-09-01 00:34:31 -0400

    ajax complete

--- app.js
+++ app.js
@@ -6,7 +6,8 @@
 $ (() => {
 
 
-for (var i = 1; i < 150; i++) {
+
+for (let i = 1; i <= 30; i++) {
 
 
  $.ajax({
@@ -17,26 +18,43 @@ for (var i = 1; i < 150; i++) {
    }
  ).then(
    (data) => {
-    const pokemon = []
-    pokemon['name'] = data.name;
-    pokemon['id']= data.id;
-    pokemon['image'] = data.sprites['front_default']
-// console.log(data);
-const displayPokemon = () => {
-  $image = $('<img>').attr('src', pokemon.image)
-  $('#pokedex').append($image)
-  $name = $('<li class = name>').text(pokemon.name)
-$('#pokedex').append($name)
-  $id = $('<li class = id>').text(pokemon.id)
-  $('#pokedex').append($id)
 
+    const pokemon = []
+    pokemon['image'] = data.sprites['front_default']
+    pokemon['name'] = data.name;
+    pokemon['move1']= data.moves[0].move.name
+    pokemon['move2']= data.moves[1].move.name
+    pokemon['move3']= data.moves[2].move.name
+    // pokemon['experience']= data.base_experience;
+  // console.log(data.moves[1].move.name);
+// console.log(data);
+const showPokemon = () => {
+  $image = $('<img class ="pokeball">').attr('src', pokemon.image)
+  $('#list').append($image)
+  $name = $('<div class = name>').text('NAME: ' + pokemon.name)
+$('#list').append($name)
+  // $experience = $('<li class = experience>').text('expo ' + pokemon.experience)
+  // $('#list').append($experience)
+ $move1 = $('<li class = moves>').text('Attack: ' + pokemon.move1 +',  '+pokemon.move2)
+ $('#list').append($move1)
+ // $move2 = $('<li class = moves>').text( pokemon.move2)
+ // $('#list').append($move2)
+ $move3 = $('<li class = moves>').text('Special Attack: '+pokemon.move3)
+ $('#list').append($move3)
 }
 
-displayPokemon()
+$('.pokeball').on('click', (event) =>{
+  // console.log($(event.currentTarget));
+  console.log('hi');
+  })
+
+
+showPokemon()
 
 })
 
 }
+
 
 
 
